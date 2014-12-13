21cb337fd207e6bcb7298a27ab4af9320e9a3f4c
sowjanyaboyapati26 <brsowju.26@gmail.com>
2014-12-12 21:54:59 -0800

    WordcountInPdf takes input file and counts and count>4 result

---
+++ WordCountinPdf/src/WordCountReducer.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+ 
+public class WordCountReducer<KEY> extends Reducer<KEY, LongWritable,
+                                                 KEY,LongWritable> {
+ 
+  private LongWritable result = new LongWritable();
+ 
+  public void reduce(KEY key, Iterable<LongWritable> values,
+                     Context context) throws IOException, InterruptedException {
+    long sum = 0;
+    for (LongWritable val : values) {
+      sum += val.get();
+    }
+    if (sum>4)
+    result.set(sum);
+    
+    context.write(key, result);
+  }
+ 
+}
+
